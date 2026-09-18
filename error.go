aabda6eb3a76e1ccdc270fcfd43ba54e875eaf7e
Igor Derkach <gosha753951@gmail.com>
2026-09-18 17:29:07 +0400

    fix: working with HTTP responses in errors

--- error.go
+++ error.go
@@ -5,15 +5,21 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"unicode/utf8"
 )
+
+// bodyReadLimit is the maximum amount of the response body bytes that is included to an error message
+const bodyReadLimit = 256
 
 type TusError struct {
 	inner error
-
-	msg string
+	msg   string
 }
 
 func (te TusError) Error() string {
+	if te.inner == nil {
+		return te.msg
+	}
 	return fmt.Sprintf("%s: %s", te.msg, te.inner)
 }
 
@@ -26,28 +32,36 @@ func (te TusError) Is(e error) bool {
 	return ok && v.msg == te.msg || errors.Is(te.inner, e)
 }
 
-func (te TusError) WithErr(err error) TusError {
-	te.inner = err
+func newTusErrorWithErr(te TusError, inner error) TusError {
+	te.inner = inner
 	return te
 }
 
-func (te TusError) WithText(s string) TusError {
-	te.inner = errors.New(s)
-	return te
-}
-
-func (te TusError) WithResponse(r *http.Response) TusError {
+func newTusErrorWithResponse(te TusError, r *http.Response) TusError {
 	if r == nil {
 		te.inner = fmt.Errorf("response is nil")
 		return te
 	}
 
-	b := make([]byte, 256)
-	if l, err := io.ReadFull(r.Body, b); err == nil || err == io.EOF {
-		if l > 0 {
-			te.inner = fmt.Errorf("HTTP %d: %s", r.StatusCode, b[:l])
+	b := make([]byte, bodyReadLimit)
+	if l, err := io.ReadFull(r.Body, b); err == nil || errors.Is(err, io.EOF) || errors.Is(err, io.ErrUnexpectedEOF) {
+		size := r.Header.Get("Content-Length")
+		if size == "" {
+			size = "?"
+		}
+		var body string
+		if !utf8.Valid(b[:l]) {
+			body = fmt.Sprintf("%v", b[:l]) // if the body is not valid UTF-8, print it as a byte slice
 		} else {
-			te.inner = fmt.Errorf("HTTP %d: <no body>", r.StatusCode)
+			body = string(b[:l])
+		}
+		switch {
+		case err == nil:
+			te.inner = fmt.Errorf("HTTP %d: body (%s bytes): %s... (truncated)", r.StatusCode, size, body)
+		case l > 0:
+			te.inner = fmt.Errorf("HTTP %d: body (%s bytes): %s", r.StatusCode, size, body)
+		default:
+			te.inner = fmt.Errorf("HTTP %d: empty body", r.StatusCode)
 		}
 	} else {
 		te.inner = fmt.Errorf("HTTP %d: read body: %w", r.StatusCode, err)
--- stream.go
+++ stream.go
@@ -376,13 +376,13 @@ func (us *UploadStream) uploadChunkImpl(requestURL string, data io.Reader, extra
 	switch response.StatusCode {
 	case http.StatusCreated: // For "Creation With Upload" feature
 		if us.uploadMethod != http.MethodPost {
-			err = ErrUnexpectedResponse
+			err = newTusErrorWithResponse(ErrUnexpectedResponse, response)
 			return
 		}
 		fallthrough
 	case http.StatusNoContent:
 		if offset, err = strconv.ParseInt(response.Header.Get("Upload-Offset"), 10, 64); err != nil {
-			err = ErrProtocol.WithErr(fmt.Errorf("cannot parse Upload-Offset header %q: %w", response.Header.Get("Upload-Offset"), err))
+			err = newTusErrorWithErr(ErrProtocol, fmt.Errorf("cannot parse Upload-Offset header %q: %w", response.Header.Get("Upload-Offset"), err))
 			return
 		}
 		bytesUploaded = offset - us.Upload.RemoteOffset
@@ -392,27 +392,27 @@ func (us *UploadStream) uploadChunkImpl(requestURL string, data io.Reader, extra
 		if v := response.Header.Get("Upload-Expires"); v != "" {
 			var t time.Time
 			if t, err = time.Parse(time.RFC1123, v); err != nil {
-				err = ErrProtocol.WithErr(fmt.Errorf("cannot parse Upload-Expires RFC1123 header %q: %w", v, err))
+				err = newTusErrorWithErr(ErrProtocol, fmt.Errorf("cannot parse Upload-Expires RFC1123 header %q: %w", v, err))
 				return
 			}
 			us.Upload.UploadExpired = &t
 		}
 	case http.StatusConflict:
-		err = ErrOffsetsNotSynced.WithResponse(response)
+		err = newTusErrorWithResponse(ErrOffsetsNotSynced, response)
 	case http.StatusForbidden:
-		err = ErrCannotUpload.WithResponse(response)
+		err = newTusErrorWithResponse(ErrCannotUpload, response)
 	case http.StatusNotFound, http.StatusGone:
-		err = ErrUploadDoesNotExist.WithResponse(response)
+		err = newTusErrorWithResponse(ErrUploadDoesNotExist, response)
 	case http.StatusRequestEntityTooLarge:
-		err = ErrUploadTooLarge.WithResponse(response)
+		err = newTusErrorWithResponse(ErrUploadTooLarge, response)
 	case 460: // Non-standard HTTP code '460 Checksum Mismatch'
 		if us.checksumHash != nil {
-			err = ErrChecksumMismatch.WithResponse(response)
+			err = newTusErrorWithResponse(ErrChecksumMismatch, response)
 			return
 		}
 		fallthrough
 	default:
-		err = ErrUnexpectedResponse
+		err = newTusErrorWithResponse(ErrUnexpectedResponse, response)
 	}
 	return
 }
